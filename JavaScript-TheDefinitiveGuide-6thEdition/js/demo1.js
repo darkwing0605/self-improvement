3d764902e9b2181498f53f653c8ecdf8f6b00403
未央 紫荆 <2553292615@qq.com>
2017-07-25 21:23:20 +0800

    20170725

---
+++ JavaScript-TheDefinitiveGuide-6thEdition/js/demo1.js
@@ -0,0 +1,25 @@
+var example1 = Math.pow(2,53);
+var example2 = Math.round(.6);
+var example3 = Math.ceil(.6);
+var example4 = Math.floor(.6);
+var example5 = Math.abs(-5);
+var example6 = Math.max(5,3,9);
+var example7 = Math.min(5,3,9);
+var example8 = Math.random();
+var example9 = Math.PI;
+var example10 = Math.E;
+var example11 = Math.sqrt(3);
+var example12 = Math.pow(3,1/3);
+var example13 = Math.sin(0);
+var example14 = Math.log(10);
+var example15 = Math.log(100)/Math.LN10;
+var example16 = Math.log(512)/Math.LN2;
+var example17 = Math.exp(3);
+
+var example18 = .3 - .2;
+var example19 = .2 - .1;
+
+var example20 = new Date(2017, 0, 1);
+var example21 = new Date(2017, 0, 1, 17, 10, 30);
+var example22 = new Date;
+var example23 = example20 - example21; //毫秒数
